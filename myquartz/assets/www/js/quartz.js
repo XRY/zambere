7ba74251c6e021624bc4ae713d8f02f3e252ae17
henry14 <henrytebs@gmail.com>
2013-03-19 17:21:52 +0300

    insertion modified

--- myquartz/assets/www/js/quartz.js
+++ myquartz/assets/www/js/quartz.js
@@ -1,46 +1,37 @@
 /*Created by Alex Nyika
  * The main function file 
  */
- 
 	var employees;
 	var markedEmployees;
 	var employeeid;
 	var flags;
 	var x = 0;
-<<<<<<< HEAD
 	var submitions;
-=======
->>>>>>> origin/HEAD
- function vibrate() {// vibrate for 50 micro seconds
+
+ 	function vibrate() {// vibrate for 50 micro seconds
         navigator.notification.vibrate(50);
-        
         }
  
- function vibrateBeep()
- {
- vibrate();
- playBeep();
- 
- }
+ 	function vibrateBeep(){
+ 		vibrate();
+ 		playBeep();
+ 		}
 
     // Beep Once
     function playBeep() {
         navigator.notification.beep(1);
-    }
+    	}
 
-  function createDB()
-  {
+  	function createDB(){
     // var db = window.sqlitePlugin.openDatabase({name: "/mnt/sdcard/Quartz/quartz"});
- 
-	  var db = window.sqlitePlugin.openDatabase({name: "quartz"});
- }
+	  	var db = window.sqlitePlugin.openDatabase({name: "quartz"});
+ 		}
 
 	function errorCB(err) {
     	alert("Error processing SQL: "+err.code);
-	}
+		}
 
-	function getVar(name)
-	{
+	function getVar(name){
     	get_string = document.location.search;         
          return_value = '';
          
@@ -78,61 +69,55 @@
         } 
           
       function clickSave(){
-        
-       vibrate();
-       } 
+      	 vibrate();
+      	} 
  
- function queryDb2()
-  {
-  	if(x<employees.length-1){
-  		flags[x] = 1;
-  		x++;
-  		document.getElementById("demo").innerHTML=employees[x];
-  	}
-  	return;
-  }
+ 	  function queryDb2(){
+  		if(x<employees.length-1){
+  			flags[x] = 1;
+  			x++;
+  			document.getElementById("demo").innerHTML=employees[x];
+  			}
+  			return;
+  		}
   
-  function queryDb3(){
-  	if(x<employees.length-1){
-  		flags[x] = 0;
-  		x++;
-  		document.getElementById("demo").innerHTML=employees[x];
-  	}
-  	return;
-  }
-   function queryDb()
-  	{
+  	function queryDb3(){
+  		if(x<employees.length-1){
+  			flags[x] = 0;
+  			x++;
+  			document.getElementById("demo").innerHTML=employees[x];
+  			}
+  		return;
+  		}
+   function queryDb(){
       //var db = window.sqlitePlugin.openDatabase({name: "/mnt/sdcard/Quartz/quartz"});
-       var db = window.sqlitePlugin.openDatabase({name: "quartz"});
+        var db = window.sqlitePlugin.openDatabase({name: "quartz"});
   		db.transaction(returnEmplyees, errorCB);
   		document.getElementById("demo").innerHTML=employees[x];
   		flags = new Array(employees.length);
-  		for(var d=0; d<flags.length; d++)
-  		{
-  			flags[d] = 0;
-  			
+  		for(var d=0; d<flags.length; d++){
+  			flags[d] = 0;	
+  			}
+  			markedEmployees = new Array();
+  			markedEmployees.push(employeeid[x]);
   		}
-  		markedEmployees = new Array();
-  		markedEmployees.push(employeeid[x]);
-  }
   
-  function errorCB(err) {
-    alert("Error processing SQL: "+err.code);
-}
-<<<<<<< HEAD
+  	function errorCB(err) {
+   		 alert("Error processing SQL: "+err.code);
+		}
     
     function returnEmplyees(tx){
     	employees = new Array();
     	employeeid = new Array();
 		tx.executeSql("select * from workers;", [], function(tx, res) {
-     	var len = res.rows.length;
-    	for (var i=0; i<len; i++){
-        	employees[i] = res.rows.item(i).name;
-        	employeeid[i] = res.rows.item(i).id;
-        }
-    //alert(employees.length);  
-    });
-    }
+     		var len = res.rows.length;
+    		for (var i=0; i<len; i++){
+        		employees[i] = res.rows.item(i).name;
+        		employeeid[i] = res.rows.item(i).id;
+        		}
+    			//alert(employees.length);  
+    		});
+    	}
     
     function doInsertion()
     {
@@ -143,102 +128,47 @@
     	x = 0;
     }
     
-    function checkInsertion()
-    {
+    function checkInsertion(){
     	var db = window.sqlitePlugin.openDatabase({name: "quartz"});
+    	
     	db.transaction(confirmDbEntries, errorCB);
     	
-    	/*var htmlString = "<p>";
-    	for(var t=0; t<submitions.length; t++){
-    	htmlString = htmlString + "<p>"+ submitions[t] +"</p>"
-    	//'<h>'+submitions[t]; +'</h>'
-    	//document.getElementById("demo2").innerHTML=submitions[1];
-    	}
-    	htmlString = htmlString + "</p>";
-    	$('demo2').html(htmlString);
-    	*/
     	//document.getElementById("demo2").innerHTML=submitions[1];
     	var output;
     	for(var n = 0; n < submitions.length; n++){
-    	//alert(submitions[n]);
+    		//alert(submitions[n]);
     		output += '<p>'+submitions[n]+'</p>';
-    	}
+    		}
     	//output += '<a href="#0.2_page1"> Done </a>';
     	document.body.innerHTML = output;
-    }
-    
-    function insertWorkers(tx)
-    {
-    	var date = new Date();
-  		var hour = date.getHours();
-  		var day = date.getDate();
-  		var month = date.getMonth()+1;
-  		var year = date.getFullYear();
-  		var completeDate = day +"/"+ month +"/" + year;
-  		//alert(completeDate);
-    	//tx.executeSql('CREATE TABLE IF NOT EXISTS attendence (id integer primary key autoincrement not null unique, date text, morning integer, afternoon integer)');
-    	tx.executeSql('CREATE TABLE IF NOT EXISTS attendance (id integer primary key autoincrement, date VARCHAR, employeeid integer, morning integer, afternoon integer, toolid integer)');
-    	for (var i=0; i<employeeid.length;i++){
-    		
-    		tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES ('+completeDate+', '+employeeid[i]+', '+flags[i]+', 0)');
-    		alert(flags[i]);
-    		//tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES ('+completeDate+', '+employeeid[i]+', 1, 0)');
-    		
-    		//alert(completeDate);
     	}
-    	
-
-    }
     
-    function testDbEntries(tx)
-    {
-    var submitions = new Array();
+    
+    function confirmDbEntries(tx){
+    	submitions = new Array();
     	tx.executeSql("select * from attendance;", [], function(tx, res) {
-     	var len = res.rows.length;
-    	for (var i=0; i<len; i++){
-        	//alert(employeeid);
-        	submitions[i] = res.rows.item(i).name;
+     		var len = res.rows.length;
+    		for (var i=0; i<len; i++){
+        		//alert(employeeid);
+        		submitions[i] = res.rows.item(i).id +' '+ res.rows.item(i).date + ' '+ res.rows.item(i).employeeid +' '+res.rows.item(i).morning;
+        		}
+        	});
         }
-        //alert(submitions.length);  
-    });
-    }
     
-    function confirmDbEntries(tx)
-    {
-    submitions = new Array();
-    	tx.executeSql("select * from attendance;", [], function(tx, res) {
-     	var len = res.rows.length;
-    	for (var i=0; i<len; i++){
-        	//alert(employeeid);
-        	submitions[i] = res.rows.item(i).id +' '+ res.rows.item(i).date + ' '+ res.rows.item(i).employeeid +' '+res.rows.item(i).morning;
-=======
+    	function returnEmplyees(tx){
+    		employees = new Array();
+    		employeeid = new Array();
+			tx.executeSql("select * from workers;", [], function(tx, res) {
+     			var len = res.rows.length;
+    			for (var i=0; i<len; i++){
+        			employees[i] = res.rows.item(i).name;
+        			employeeid[i] = res.rows.item(i).id;
+        			}
+        		//alert(submitions.length);  
+    			});
+    		}
     
-    function returnEmplyees(tx){
-    	employees = new Array();
-    	employeeid = new Array();
-		tx.executeSql("select * from workers;", [], function(tx, res) {
-     	var len = res.rows.length;
-    	for (var i=0; i<len; i++){
-        	employees[i] = res.rows.item(i).name;
-        	employeeid[i] = res.rows.item(i).id;
->>>>>>> origin/HEAD
-        }
-        //alert(submitions.length);  
-    });
-    }
-<<<<<<< HEAD
-=======
-    
-    function doInsertion()
-    {
-    	//alert("am tyt");
-    	var db = window.sqlitePlugin.openDatabase({name: "quartz"});
-    	db.transaction(insertWorkers);
-    	//db.transaction(testDbEntries, errorCB);
-    }
-    
-    function insertWorkers(tx)
-    {
+    function insertWorkers(tx){
     	var date = new Date();
   		var hour = date.getHours();
   		var day = date.getDate();
@@ -250,23 +180,14 @@
     	tx.executeSql('CREATE TABLE IF NOT EXISTS attendance (id integer primary key autoincrement, date text, employeeid integer, morning integer, afternoon integer)');
     	for (var i=0; i<employeeid.length;i++){
     		//alert(flags.length);
-    		tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES ('+completeDate+', '+employeeid[i]+', '+ flags[i] +', 0)');
+    		var result = flags[i];
+    		var finalEmployeeid = employeeid[i];
+    		var newDate = completeDate;
+    		tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES ('+newDate+', '+finalEmployeeid+', '+result+', 0)');
     		//tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES ('+completeDate+', '+employeeid[i]+', 1, 0)');
+    		}
     	}
-    	
-    	//tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES (completeDate, 2, 1, 0)');
-   		// tx.executeSql('INSERT INTO attendance (date , employeeid, morning, afternoon) VALUES (completeDate, 3, 1, 0)');
-    }
     
-   /* function testDbEntries(tx)
-    {
-    	tx.executeSql("select * from attandance;", [], function(tx, res) {
-     	var len = res.rows.length;
-    	for (var i=0; i<len; i++){
-        	alert(employeeid);
-        }
-    }*/
->>>>>>> origin/HEAD
     /*function getMorningEmployees(tx){
     var date = new Date();
   	var hour = date.getHours();
